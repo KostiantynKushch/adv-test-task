55badbf7d3e62d7c48ec963a990b057f7808536b
kostiantyn.kushch <kostiantyn.kushch@beetroot.se>
2020-08-04 18:50:50 +0300

    data sample

--- js/app.js
+++ js/app.js
@@ -3,3 +3,7 @@
 const allDishes = document.querySelectorAll('[data-dish]');
 
 console.log(allDishes);
+console.log(allDishes[0]);
+console.log('Dish Category: ' + allDishes[0].dataset.dish);
+console.log(allDishes[0].querySelector('[data-title]').innerHTML);
+console.log(allDishes[0].querySelector('[data-price]').dataset.price);
